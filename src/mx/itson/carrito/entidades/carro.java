944fd6000a202ea4315f8057d213644e4948e9f5
CHOPER <CHOPER@10.232.49.81>
2025-03-06 09:18:48 -0700

    Creacion de proyecto

---
+++ src/mx/itson/carrito/entidades/carro.java
@@ -0,0 +1,89 @@
+
+package mx.itson.carrito.entidades;
+
+/**
+ *
+ * @author CHOPER
+ */
+public class carro 
+{
+
+    /**
+     * @return the kilometraje
+     */
+    public double getKilometraje() {
+        return kilometraje;
+    }
+
+    /**
+     * @param kilometraje the kilometraje to set
+     */
+    public void setKilometraje(double kilometraje) {
+        this.kilometraje = kilometraje;
+    }
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the año
+     */
+    public int getAño() {
+        return año;
+    }
+
+    /**
+     * @param año the año to set
+     */
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    /**
+     * @return the marca
+     */
+    public String getMarca() {
+        return marca;
+    }
+
+    /**
+     * @param marca the marca to set
+     */
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+        private String color;
+        
+        private int año;
+        
+        private String marca;
+        
+        private double kilometraje;
+        
+        /**
+         * Sirve para calcular el tiempo en que tarda en recorrer una distancia.
+         * @param distancia Distancia que recorre el carro.
+         * @param velocidad Velocidad a la que viaja el carro,
+         * @return  Tiempo en el que llegará el carro.
+         */
+        public double calcularTiempo(double distancia, double velocidad)
+        {
+            this.kilometraje += distancia;
+            // La linea de arriba es lo mismo que esta de abajo
+            // this.kilometraje = this.kilometraje + distancia;
+            double tiempo = distancia / velocidad;
+            return tiempo;
+        }
+            
+}
---
+++ src/mx/itson/carrito/ui/Main.java
@@ -0,0 +1,37 @@
+
+package mx.itson.carrito.ui;
+
+import java.util.Scanner;
+import mx.itson.carrito.entidades.carro;
+
+/**
+ *
+ * @author CHOPER
+ */
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Ingresa la distancia recorrida: ");
+        
+        double distancia = sc.nextDouble();
+        
+        System.out.println("Ingresa la velocidad a la que viaja el carro: ");
+        
+        double velocidad = sc.nextDouble();
+        
+        // Generamos una instancia de la clase carro
+        carro bmw = new carro();
+        bmw.setMarca("BMW");
+        bmw.setColor("Negro");
+        bmw.setAño(2021);
+        bmw.setKilometraje(10000);
+        //Invocamos al metodo contenido en la clase carro
+      double tiempo =  bmw.calcularTiempo(distancia, velocidad);
+        
+        System.out.println("El tiempo que le tomara al carro " + bmw.getMarca() + " de color " + bmw.getColor() + " y cuyo modelo es " + bmw.getAño() + " le tomo " +  tiempo 
+        + " y ahora su kilometraje es de " + bmw.getKilometraje());
+    }
+}
